9a2d49acbf7554c7745325dc49e096887b54565a
HemangiManduskar <hemangimanduskar@gmail.com>
2017-07-04 17:16:55 -0700

    First Commit

---
+++ src/main/java/com/niit/BagShoppy/Category.java
@@ -0,0 +1,29 @@
+package com.niit.BagShoppy;
+
+public class Category {
+String catid,catname,catdescriptn;
+
+public String getCatid() {
+	return catid;
+}
+
+public void setCatid(String catid) {
+	this.catid = catid;
+}
+
+public String getCatname() {
+	return catname;
+}
+
+public void setCatname(String catname) {
+	this.catname = catname;
+}
+
+public String getCatdescriptn() {
+	return catdescriptn;
+}
+
+public void setCatdescriptn(String catdescriptn) {
+	this.catdescriptn = catdescriptn;
+}
+}
---
+++ src/main/java/com/niit/BagShoppy/Customer.java
@@ -0,0 +1,53 @@
+package com.niit.BagShoppy;
+
+public class Customer {
+String cusid,cname,cuspswrd,cusemail,cusadd,cusph;
+
+public String getCusid() {
+	return cusid;
+}
+
+public void setCusid(String cusid) {
+	this.cusid = cusid;
+}
+
+public String getCname() {
+	return cname;
+}
+
+public void setCname(String cname) {
+	this.cname = cname;
+}
+
+public String getCuspswrd() {
+	return cuspswrd;
+}
+
+public void setCuspswrd(String cuspswrd) {
+	this.cuspswrd = cuspswrd;
+}
+
+public String getCusemail() {
+	return cusemail;
+}
+
+public void setCusemail(String cusemail) {
+	this.cusemail = cusemail;
+}
+
+public String getCusadd() {
+	return cusadd;
+}
+
+public void setCusadd(String cusadd) {
+	this.cusadd = cusadd;
+}
+
+public String getCusph() {
+	return cusph;
+}
+
+public void setCusph(String cusph) {
+	this.cusph = cusph;
+}
+}
